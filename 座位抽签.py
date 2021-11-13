883b81c89ff175615949ce191d20668bc8dd87f7
walksQAQ <2878449206@qq.com>
2021-11-13 18:06:05 +0800

    1

--- 座位抽签.py
+++ 座位抽签.py
@@ -1,15 +1,12 @@
 import random
 
-T = 1
-while T <= 43:
-    a = random.randint(1,6)
-    if a != 2:
-        b = random.randint(1,7)
-    else:
-        b = random.randint(1,8)
-    d = str(a)+ " " + str(b)+ " "
-    c = str(d)
-    fo = open( c ,"w+", encoding='utf-8')
-    fo.write("yes")
-    T = T + 1
-fo.close()
+a = random.randint(1,6)
+if a != 2:
+    b = random.randint(1,7)
+else:
+    b = random.randint(1,8)
+d = str(a)+ " " + str(b)+ " "
+c = str(d)
+fo = open( c ,"w+", encoding='utf-8')
+fo.write("yes")
+T = T + 1
