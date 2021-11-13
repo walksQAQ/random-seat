10ae1cd28210ed6ac8f56a29881c1e5e4997c0cb
walksQAQ <2878449206@qq.com>
2021-11-13 18:02:54 +0800

    1

--- 座位抽签.py
+++ 座位抽签.py
@@ -1,24 +1,15 @@
 import random
 
 T = 1
-fo = open('result.txt',mode="w+")
-fo.seek(0,0)
 while T <= 43:
     a = random.randint(1,6)
     if a != 2:
         b = random.randint(1,7)
     else:
         b = random.randint(1,8)
-    d = str(a)+ " " + str(b)
-    A = 1
-    while A<= T:
-        line = fo
-        if d != line:
-            print(d)
-            fo.seek(0,2)
-            fo.write(d)
-            continue
-        A = A + 1
-        continue
+    d = str(a)+ " " + str(b)+ " "
+    c = str(d)
+    fo = open( c ,"w+", encoding='utf-8')
+    fo.write("yes")
     T = T + 1
 fo.close()
