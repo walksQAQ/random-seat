bb2f7c5d316223ecf9274364a69bb568664b2148
walksQAQ <2878449206@qq.com>
2021-11-13 18:59:03 +0800

    1

--- randomseat.py
+++ randomseat.py
@@ -1,12 +1,17 @@
 import random
 
-a = random.randint(1,6)
-if a != 2:
-    b = random.randint(1,7)
-else:
-    b = random.randint(1,8)
-d = str(a)+ " " + str(b)+ " "
-c = str(d)
-fo = open( c ,"w+", encoding='utf-8')
-fo.write("yes")
-T = T + 1
+l = "false"
+while l == "false":
+    a = random.randint(1,6)
+    if a != 2:
+        b = random.randint(1,7)
+    else:
+        b = random.randint(1,8)
+    d = str(a)+ " " + str(b)+ " "
+    c = str(d)
+    fo = open( c ,"w+", encoding='utf-8')
+    e = fo.read()
+    f = str(e)
+    if f != "yes":
+        fo.write("yes")
+        l = "true"
